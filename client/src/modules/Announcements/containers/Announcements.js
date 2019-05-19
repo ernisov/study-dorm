2b529e89681caa86bd6717b209d29462c17ec74d
Daniyar Erkinov <dan4111k1@gmail.com>
2019-05-20 03:34:57 +0600

    [client] add deleteAnnouncement import

--- client/src/modules/Announcements/containers/Announcements.js
+++ client/src/modules/Announcements/containers/Announcements.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { loadAnnouncements } from '../redux/actions';
+import { loadAnnouncements, deleteAnnouncement } from '../redux/actions';
 import { List, Button } from 'antd';
 import './Announcements.css';
 
