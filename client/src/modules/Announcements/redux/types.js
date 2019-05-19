c32ed86d306a41fa168c5824e03b197c4819f6af
Daniyar Erkinov <dan4111k1@gmail.com>
2019-05-20 03:13:42 +0600

    [client] add delete announcement types

--- client/src/modules/Announcements/redux/types.js
+++ client/src/modules/Announcements/redux/types.js
@@ -1,3 +1,6 @@
 export const LOAD_ANNOUNCEMENTS = 'load_announcements';
 export const LOAD_ANNOUNCEMENTS_FAIL = 'load_announcements_fail';
 export const SET_LOADING = 'loading_announcements';
+export const DELETE_ANNOUNCEMENT_SUCCESS = 'delete_announcement_success'
+export const DELETE_ANNOUNCEMENT_FAIL = 'delete_announcement_fail'
+export const ANNOUNCEMENT_UPDATED = 'announcement_updated'
