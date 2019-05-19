2067ffe212e99fa592b7097493c324b3b053b832
Daniyar Erkinov <dan4111k1@gmail.com>
2019-05-20 01:59:59 +0600

    [client] add load announcements action

--- client/src/modules/Announcements/redux/actions.js
+++ client/src/modules/Announcements/redux/actions.js
@@ -1,1 +1,25 @@
+import {
+  LOAD_ANNOUNCEMENTS,
+  LOAD_ANNOUNCEMENTS_FAIL,
+  SET_LOADING
+} from './types';
 import { request } from '../../../api/requests';
+
+export const loadAnnouncements = (page) => {
+  return dispatch => {
+    dispatch({ type: SET_LOADING });
+    request({
+      method: 'get',
+      url: '/announcements/',
+      params: {
+        limit: 10,
+        page: page
+      }
+    }).then((response) => {
+      dispatch({ type: LOAD_ANNOUNCEMENTS, payload: response.data });
+    }).catch(err => {
+      console.log(err);
+      dispatch({ type: LOAD_ANNOUNCEMENTS_FAIL });
+    });
+  };
+};
