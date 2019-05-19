95e5c2fda32deb75c50f79aa23c97ea3e8946817
Daniyar Erkinov <dan4111k1@gmail.com>
2019-05-20 03:15:06 +0600

    [client] add delete announcement action

--- client/src/modules/Announcements/redux/actions.js
+++ client/src/modules/Announcements/redux/actions.js
@@ -1,7 +1,10 @@
 import {
   LOAD_ANNOUNCEMENTS,
   LOAD_ANNOUNCEMENTS_FAIL,
-  SET_LOADING
+  SET_LOADING,
+  DELETE_ANNOUNCEMENT_SUCCESS,
+  DELETE_ANNOUNCEMENT_FAIL,
+  ANNOUNCEMENT_UPDATED
 } from './types';
 import { request } from '../../../api/requests';
 
@@ -22,4 +25,22 @@ export const loadAnnouncements = (page) => {
       dispatch({ type: LOAD_ANNOUNCEMENTS_FAIL });
     });
   };
+};
+
+export const deleteAnnouncement = (id) => {
+  return dispatch => {
+    request({
+      method: 'delete',
+      url: `/announcements/${id}`
+    }).then((res) => {
+      dispatch({ type: DELETE_ANNOUNCEMENT_SUCCESS, payload: res.data });
+    }).catch(err => {
+      console.log(err);
+      dispatch({ type: DELETE_ANNOUNCEMENT_FAIL });
+    });
+  };
+};
+
+export const updateAnnouncement = (old, newAnnouncement) => {
+  return { type: ANNOUNCEMENT_UPDATED, payload: { old, newUser }};
 };
