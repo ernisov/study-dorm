795e57946b6887b3ef3479f301c7daf7a56d858b
Daniyar Erkinov <dan4111k1@gmail.com>
2019-05-20 02:01:15 +0600

    [client] add list in announcements class

---
+++ client/src/modules/Announcements/containers/Announcements.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loadAnnouncements } from '../redux/actions';
+import { List, Button } from 'antd';
+// import './Announcements.css';
+
+class Announcements extends Component {
+  componentDidMount() {
+    if (this.props.page === 1 && this.props.announcements.length === 0) {
+      this.props.loadAnnouncements(this.props.page);
+    }
+  }
+
+  render() {
+    const { announcements, loading, hasNextPage } = this.props;
+    const loadMore = hasNextPage ? (
+      <div
+        style={{
+          textAlign: 'center',
+          marginTop: 12,
+          height: 32,
+          lineHeight: '32px',
+        }}
+      >
+        <Button
+          onClick={() => this.props.loadAnnouncements(this.props.page)}
+        >
+          load more
+        </Button>
+      </div>
+    ) : null;
+
+    return (
+      <div className="Announcements">
+        <h3>Announcements</h3>
+        <List
+          className='announcements-list'
+          loading={(loading && hasNextPage)}
+          itemLayout='horizontal'
+          loadMore={loadMore}
+          dataSource={announcements}
+          renderItem={item => {
+            // let edit = (
+            //   <Link to={{
+            //
+            //   }}>
+            //     edit
+            //   </Link>
+            // );
+            //
+            // let deleteAnnouncement = (
+            //   <span onClick={() => this.props.deleteAnnouncement(item)}>
+            //     delete
+            //   </span>
+            // );
+
+            return (
+              <List.Item actions={[edit, deleteAnnouncement]}>
+                <List.Item.Meta
+                  title={`${item.title}`}
+                  description={item.description}/>
+              </List.Item>
+            );
+          }} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const {
+    announcements,
+    page,
+    hasNextPage,
+    hasPrevPage,
+    totalDocs,
+    totalPages,
+    loading
+  } = state.announcements;
+
+  return {
+    announcements,
+    page,
+    hasNextPage,
+    hasPrevPage,
+    totalDocs,
+    totalPages,
+    loading
+  }
+}
+
+export default connect(mapStateToProps, { loadAnnouncements })(Announcements);
