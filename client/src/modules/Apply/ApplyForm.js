fabbd171a02348b84e82c7211ebe58c9a81ec452
Sultan Ernisov <ernisov@gmail.com>
2019-05-21 06:26:42 +0600

    Add application form input validations

--- client/src/modules/Apply/ApplyForm.js
+++ client/src/modules/Apply/ApplyForm.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import { Form, Input, Select, DatePicker,Button,Checkbox } from 'antd';
+import {
+  Form,
+  Input,
+  Select,
+  DatePicker,
+  Button,
+  Checkbox
+} from 'antd';
 import moment from 'moment';
+import { request } from '../../api/requests';
 import './ApplyForm.css';
 
 const { MonthPicker, RangePicker } = DatePicker;
-const dateFormat = 'YYYY/MM/DD';
-const monthFormat = 'YYYY/MM';
 const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY'];
 
 class ApplyForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      birthDate: moment('01/01/2015', dateFormatList[0]),
+      issuedDate: moment('01/01/2015', dateFormatList[0]),
+      passportNumber: '',
+      passportNumberInvalid: false,
+      issuedBy: '',
+      issuedByInvalid: false,
       agreed: false
     };
+    this.handleBirthDate = this.handleBirthDate.bind(this);
     this.onAgreementChange = this.onAgreementChange.bind(this);
+    this.handlePassportNumber = this.handlePassportNumber.bind(this);
+    this.handleIssuedBy = this.handleIssuedBy.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleBirthDate(date, dateString) {
+    this.setState({ birthDate: date });
+  }
+
+  handleIssuedDate(date, dateString) {
+    this.setState({ issuedDate: date });
+  }
+
+  handlePassportNumber(e) {
+    this.setState({
+      passportNumber: e.target.value,
+      passportNumberInvalid: false
+    });
+  }
+
+  handleIssuedBy(e) {
+    this.setState({
+      issuedBy: e.target.value,
+      issuedByInvalid: false
+    });
   }
 
   onAgreementChange(e) {
@@ -24,6 +61,22 @@ class ApplyForm extends Component {
 
   onSubmit(e) {
     e.preventDefault();
+    if (this.state.passportNumber.length !== 7) {
+      this.setState({ passportNumberInvalid: true });
+    }
+    if (!this.state.issuedBy) {
+      this.setState({ issuedByInvalid: true });
+    }
+
+    if (!this.state.passportNumberInvalid && !this.state.issuedByInvalid) {
+      let submitObj = {
+        birthDate: this.state.birthDate.toDate(),
+        passportNumber: this.state.passportNumber,
+        issuedBy: this.state.issuedBy,
+        issuedDate: this.state.issuedDate.toDate()
+      };
+      console.log(submitObj);
+    }
   }
 
   render() {
@@ -34,6 +87,7 @@ class ApplyForm extends Component {
         md: { span: 10 }
       }
     };
+    const inputError = 'Input is required';
 
     return (
       <div className='ApplyForm'>
@@ -42,17 +96,38 @@ class ApplyForm extends Component {
           <Form {...formItemLayout}>
             <Form.Item required label='Date of birth'>
               <DatePicker
-                defaultValue={moment('01/01/2015', dateFormatList[0])}
+                onChange={this.handleBirthDate}
+                value={this.state.birthDate}
                 format={dateFormatList} />
             </Form.Item>
-            <Form.Item required label='Passport number'>
-              <Input addonBefore="AN" defaultValue="" />
+            <Form.Item
+              required
+              validateStatus={this.state.passportNumberInvalid ? 'error' : ''}
+              help={this.state.passportNumberInvalid ? inputError : null}
+              label='Passport number'
+            >
+              <Input
+                addonBefore='AN'
+                allowClear
+                value={this.state.passportNumber}
+                onChange={this.handlePassportNumber}  />
             </Form.Item>
-            <Form.Item required label='Issued by'>
-              <Input addonBefore="MKK" defaultValue="" />
+            <Form.Item
+              required
+              validateStatus={this.state.issuedByInvalid ? 'error' : ''}
+              help={this.state.issuedByInvalid ? inputError : null}
+              label='Issued by'
+            >
+              <Input
+                value={this.state.issuedBy}
+                onChange={this.handleIssuedBy}
+                addonBefore='MKK' />
             </Form.Item>
             <Form.Item required label='Issued on'>
-              <DatePicker defaultValue={moment('01/01/2015', dateFormatList[0])} format={dateFormatList} />
+              <DatePicker
+                onChange={this.handleIssuedDate}
+                value={this.state.issuedDate}
+                format={dateFormatList} />
             </Form.Item>
             <div className="">
               <p>Обязуюсь:</p>
