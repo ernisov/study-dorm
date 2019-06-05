cc0ff4ed3ac36c59f1b7c1415665bf101491bc76
Sultan Ernisov <ernisov@gmail.com>
2019-06-05 19:41:14 +0600

    redesign tenant component

--- client/src/modules/Dormitory/components/Tenant.js
+++ client/src/modules/Dormitory/components/Tenant.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Icon, Tooltip } from 'antd';
 import './Tenant.css';
 
 export default (props) => (
-  <Link to={`/tenants/${props.username}`}>
-    <div className='RoomDetails-Tenant'>
+  <div className='RoomDetails-Tenant'>
+    <div className='RoomDetails-Tenant-info'>
       <p className='RoomDetails-Tenant-fullName'>{`${props.firstName} ${props.lastName}`}</p>
-      <p className='RoomDetails-Tenant-role'>{props.role}</p>
+      <div className='RoomDetails-Tenant-row'>
+        <p className='RoomDetails-Tenant-role'>{props.role}</p>
+        <div className='RoomDetails-Tenant-actions'>
+          <Tooltip title='move'>
+            <Link className='RoomDetails-Icon' to={`/tenants/${props.username}`}>
+              <Icon type='swap' />
+            </Link>
+          </Tooltip>
+          <Tooltip title='unsettle'>
+            <Link className='RoomDetails-Icon' to={`/tenants/${props.username}`}>
+              <Icon type='close-circle' />
+            </Link>
+          </Tooltip>
+          <Tooltip title={`see ${props.firstName}'s details`}>
+            <Link className='RoomDetails-Icon' to={`/tenants/${props.username}`}>
+              <Icon type='info-circle' />
+            </Link>
+          </Tooltip>
+        </div>
+      </div>
     </div>
-  </Link>
+  </div>
 );
