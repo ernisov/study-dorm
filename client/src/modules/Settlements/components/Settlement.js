c51f085e010724b88ff265177419a93219da4ba2
Sultan Ernisov <ernisov@gmail.com>
2019-06-15 17:43:35 +0600

    add Settlement component

--- client/src/modules/Settlements/components/Settlement.js
+++ client/src/modules/Settlements/components/Settlement.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import moment from 'moment';
+import './Settlement.css';
 
-const Settlement = (props) => (
-  <div>
-    <p>{props.action}</p>
-    <p>{props.username}</p>
-  </div>
-);
+const Settlement = (props) => {
+  let text = null;
+  if (props.action === 'settle') {
+    text = (
+      <p>
+        <span>{props.username}</span>
+        was settled to
+        <span>{props.to}</span>
+      </p>
+    );
+  } else if (props.action === 'unsettle') {
+    text = (
+      <p>
+        <span>{props.username}</span>
+        was unsettled from
+        <span>{props.from}</span>
+      </p>
+    );
+  } else {
+    text = (
+      <p>
+        <span>{props.username}</span>
+        was moved
+        <span>{props.from}</span> to <span>{props.to}</span>
+      </p>
+    );
+  }
+
+  return (
+    <div className='Settlement'>
+      <div className='Settlement-info'>
+        <p>by: <span>{props.by}</span></p>
+        <p>{moment(props.date).format('DD/MM/YYYY HH:mm')}</p>
+      </div>
+      <div className='Settlement-details'>{text}</div>
+    </div>
+  );
+}
 
 export default Settlement;
